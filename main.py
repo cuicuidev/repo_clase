845247fd445d612cda3dddba77d6139b61a34dca
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-01-30 11:16:25 -0800

    He añadido un dataframe con datos de iris

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import seaborn as sns
 
 def main():
     st.write("Hola mundo!")
+    with st.expander("Dataframe"):
+        st.dataframe(sns.load_dataset("iris"))
     st.write("Adiós mundo...")
 
 if __name__ == "__main__":
